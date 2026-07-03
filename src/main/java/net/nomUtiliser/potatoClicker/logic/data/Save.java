9bde9394446155293e64a70e265a2fe7314075fd
used <billylin115@gmail.com>
2026-07-03 09:26:33 +0200

    added save upgrade

--- src/main/java/net/nomUtiliser/potatoClicker/logic/data/Save.java
+++ src/main/java/net/nomUtiliser/potatoClicker/logic/data/Save.java
@@ -15,7 +15,7 @@ public class Save {
     public BigInteger potatoCount;
 
     public Upgrade[] upgrades;
-
+    static int i = 0;
     public static @NotNull Save mkNewEmptySave() {
         Save s = new Save();
         List<AbstractUpgrade> allUpgrades = PotatoClicker.upgradesEvent.reg.getAll()
@@ -24,11 +24,14 @@ public class Save {
                                 upgrade-> !upgrade.id().getNamespace().equals(PotatoClicker.MOD_ID)
                         )
                 ).toList();
+        s.upgrades = new Upgrade[allUpgrades.size()];
         for (AbstractUpgrade u : allUpgrades) {
-
+            s.upgrades[i] = new Upgrade();
+            s.upgrades[i].id = u.getName();
+            s.upgrades[i].quantity = 0;
+            i++;
         }
         s.potatoCount = BigInteger.valueOf(0);
-        s.upgrades = new Upgrade[]{};
         return s;
     }
 }
