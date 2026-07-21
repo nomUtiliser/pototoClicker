f4a6b3e87622a172798ebce2064a8a139a4e4f66
used <billylin115@gmail.com>
2026-07-21 08:55:31 +0200

    new upgrade

---
+++ src/main/java/net/nomUtiliser/potatoClicker/upgrades/Grain.java
@@ -0,0 +1,18 @@
+package net.nomUtiliser.potatoClicker.upgrades;
+
+
+import net.minheur.potoflux.utils.ressourcelocation.ResourceLocation;
+import net.nomUtiliser.potatoClicker.PotatoClicker;
+
+import java.math.BigInteger;
+
+public class Grain extends AbstractUpgrade{
+    public Grain() {
+        super(BigInteger.valueOf(5000), "Grain de palates dorée", BigInteger.valueOf(35));
+    }
+
+    @Override
+    public ResourceLocation id() {
+        return new ResourceLocation(PotatoClicker.MOD_ID, "grain");
+    }
+}
